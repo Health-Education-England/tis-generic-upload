f993385f0d5c025d6a1500b568c87e9a4c2d5eb8
Andy Dingley <judge40@gmail.com>
2020-05-14 20:50:49 +0100

    refactor: Replace getters and setters with Lombok
    Replace the getters and setters in the XSL DTOs with Lombok generation.
    
    TISNEW-3857

--- generic-upload-api/src/main/java/com/transformuk/hee/tis/genericupload/api/dto/PlacementDeleteXLS.java
+++ generic-upload-api/src/main/java/com/transformuk/hee/tis/genericupload/api/dto/PlacementDeleteXLS.java
@@ -7,20 +7,4 @@ public class PlacementDeleteXLS extends TemplateXLS {
 
   private String placementId;
   private String placementStatus;
-
-  public String getPlacementId() {
-    return placementId;
-  }
-
-  public void setPlacementId(String placementId) {
-    this.placementId = placementId;
-  }
-
-  public String getPlacementStatus() {
-    return placementStatus;
-  }
-
-  public void setPlacementStatus(String placementStatus) {
-    this.placementStatus = placementStatus;
-  }
 }
--- generic-upload-api/src/main/java/com/transformuk/hee/tis/genericupload/api/dto/TemplateXLS.java
+++ generic-upload-api/src/main/java/com/transformuk/hee/tis/genericupload/api/dto/TemplateXLS.java
@@ -11,22 +11,6 @@ public class TemplateXLS {
   private String errorMessage;
   private boolean successfullyImported;
 
-  public int getRowNumber() {
-    return rowNumber;
-  }
-
-  public void setRowNumber(int rowNumber) {
-    this.rowNumber = rowNumber;
-  }
-
-  public String getErrorMessage() {
-    return errorMessage;
-  }
-
-  public void setErrorMessage(String errorMessage) {
-    this.errorMessage = errorMessage;
-  }
-
   public void addErrorMessage(String errorMessage) {
     this.errorMessage = this.errorMessage == null ? errorMessage
         : this.errorMessage + System.lineSeparator() + errorMessage;
@@ -40,14 +24,6 @@ public class TemplateXLS {
 
   public boolean hasErrors() {
     return !StringUtils.isEmpty(this.errorMessage);
-  }
-
-  public boolean isSuccessfullyImported() {
-    return successfullyImported;
-  }
-
-  public void setSuccessfullyImported(boolean successfullyImported) {
-    this.successfullyImported = successfullyImported;
   }
 
   public void initialiseSuccessfullyImported() {
