d141c55a1b4518e520e82e157661a21fdf38ed3f
Andy Dingley <judge40@gmail.com>
2020-05-15 11:45:05 +0100

    feat: Create ExcelColumn annotation
    Create an annotation that can be used to add information about the excel
    column associated with a particular DTO field. The annotation should
    allow setting the name of the column and whether it is required (default
    to false).
    
    TISNEW-3857

---
+++ generic-upload-api/src/main/java/com/transformuk/hee/tis/genericupload/api/ExcelColumn.java
@@ -0,0 +1,15 @@
+package com.transformuk.hee.tis.genericupload.api;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface ExcelColumn {
+
+  String name();
+
+  boolean required() default false;
+}
