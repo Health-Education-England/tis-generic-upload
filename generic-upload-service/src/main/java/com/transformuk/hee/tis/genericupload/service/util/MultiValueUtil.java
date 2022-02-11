e34db1306bf5d9b8139da669889bf816dd1993fd
Yafang Deng <hihilary@outlook.com>
2022-02-11 12:18:51 +0000

    fix: code smells

--- generic-upload-service/src/main/java/com/transformuk/hee/tis/genericupload/service/util/MultiValueUtil.java
+++ generic-upload-service/src/main/java/com/transformuk/hee/tis/genericupload/service/util/MultiValueUtil.java
@@ -9,6 +9,16 @@ public class MultiValueUtil {
 
   public static final String MULTI_VALUE_SEPARATOR = ";";
 
+  private MultiValueUtil() {
+    throw new IllegalStateException("Utility class");
+  }
+
+  /**
+   * Split string values by semicolon.
+   *
+   * @param valueToSplit the string values separated by semicolon
+   * @return the list of split string values
+   */
   public static List<String> splitMultiValueField(String valueToSplit) {
     if (valueToSplit != null && !valueToSplit.isEmpty()) {
       String[] splitValues = valueToSplit.split(MULTI_VALUE_SEPARATOR);
