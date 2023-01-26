7297716bb0923cee11d24344675a682834a39a74
Yafang Deng <hihilary@outlook.com>
2023-01-26 11:58:36 +0000

    chore:retry a stalled file
    The file is stalled may due to timeout
    
    NO-CARD

---
+++ generic-upload-service/src/main/resources/db/migration/V1.8__retry_failed_file_2.sql
@@ -0,0 +1,6 @@
+UPDATE `ApplicationType`
+SET `status` = 'PENDING'
+WHERE `id` IN(28781)
+  AND `logId` IN(1674667043653)
+  AND `fileName` = 'Med School Email Updates completed 2.xlsx'
+  AND `status` = 'IN_PROGRESS';
