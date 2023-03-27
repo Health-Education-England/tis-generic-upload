3455c1683963ca4b01052cc06d38c2e6dd4ec9d5
Yafang Deng <hihilary@outlook.com>
2023-03-27 11:22:59 +0100

    chore:update status of stalled file
    no-ticket

---
+++ generic-upload-service/src/main/resources/db/migration/V1.9__update_stalled_file.sql
@@ -0,0 +1,6 @@
+UPDATE `ApplicationType`
+SET `status` = 'UNEXPECTED_ERROR'
+WHERE `id` IN(29231)
+  AND `logId` IN(1679671066459)
+  AND `fileName` = 'TIS People Update Template - Email 1.xls'
+  AND `status` = 'IN_PROGRESS';
