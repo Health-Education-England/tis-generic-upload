947332c142fc07aa2cbf1893612ab12b2b68231e
Yafang Deng <hihilary@outlook.com>
2023-04-04 15:45:15 +0100

    fix:personUpdate validation
    TIS-SHED

--- generic-upload-service/src/main/java/com/transformuk/hee/tis/genericupload/service/service/mapper/ContactDetailsMapper.java
+++ generic-upload-service/src/main/java/com/transformuk/hee/tis/genericupload/service/service/mapper/ContactDetailsMapper.java
@@ -3,9 +3,11 @@ package com.transformuk.hee.tis.genericupload.service.service.mapper;
 import com.transformuk.hee.tis.genericupload.api.dto.PersonUpdateXls;
 import com.transformuk.hee.tis.tcs.api.dto.ContactDetailsDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ContactDetailsMapper {
 
+  @Mapping(source = "tisPersonId", target = "id")
   ContactDetailsDTO toDto(PersonUpdateXls xls);
 }
