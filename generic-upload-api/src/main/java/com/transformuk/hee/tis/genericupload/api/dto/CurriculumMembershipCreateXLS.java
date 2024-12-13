a46670e4f40f479f7a83465aab5506bc643f1627
jayanta2018 <jayanta.saha@hee.nhs.uk>
2024-12-13 15:39:38 +0000

    wip: get programme membership by uuid from tcs

--- generic-upload-api/src/main/java/com/transformuk/hee/tis/genericupload/api/dto/CurriculumMembershipCreateXLS.java
+++ generic-upload-api/src/main/java/com/transformuk/hee/tis/genericupload/api/dto/CurriculumMembershipCreateXLS.java
@@ -10,12 +10,12 @@ import lombok.EqualsAndHashCode;
 public class CurriculumMembershipCreateXLS extends TemplateXLS {
   @ExcelColumn(name = "TIS_ProgrammeMembership_ID*", required = true)
   private String tisProgrammeMembershipId;
-  @ExcelColumn(name = "Curriculum_Start_Date*", required = true)
+  @ExcelColumn(name = "Curriculum Start Date*", required = true)
   private LocalDate curriculumStartDate;
 
-  @ExcelColumn(name = "Curriculum_End_Date*", required = true)
+  @ExcelColumn(name = "Curriculum End Date*", required = true)
   private LocalDate curriculumEndDate;
 
-  @ExcelColumn(name = "Curriculum_Name*", required = true)
+  @ExcelColumn(name = "Curriculum Name*", required = true)
   private String curriculumName;
 }
--- generic-upload-service/src/main/java/com/transformuk/hee/tis/genericupload/service/service/CurriculumMembershipCreateTransformerService.java
+++ generic-upload-service/src/main/java/com/transformuk/hee/tis/genericupload/service/service/CurriculumMembershipCreateTransformerService.java
@@ -1,8 +1,50 @@
 package com.transformuk.hee.tis.genericupload.service.service;
 
+import com.transformuk.hee.tis.genericupload.api.dto.CurriculumMembershipCreateXLS;
+import com.transformuk.hee.tis.tcs.api.dto.ProgrammeMembershipDTO;
+import com.transformuk.hee.tis.tcs.client.service.impl.TcsServiceImpl;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.client.ResourceAccessException;
 
 @Component
 public class CurriculumMembershipCreateTransformerService {
 
+  private static final String DID_NOT_FIND_PROGRAMME_MEMBERSHIP_FOR_ID = "Did not find the programme membership for id \"%s\".";
+
+  @Autowired
+  private TcsServiceImpl tcsServiceImpl;
+
+  void processCurriculumMembershipCreateUpload(
+      List<CurriculumMembershipCreateXLS> curriculumMembershipCreateXLSS, String username) {
+    curriculumMembershipCreateXLSS.forEach(
+        CurriculumMembershipCreateXLS::initialiseSuccessfullyImported);
+
+    for (CurriculumMembershipCreateXLS curriculumMembershipCreateXLS : curriculumMembershipCreateXLSS) {
+      useMatchingCriteriaToCreateCurriculumMembership(curriculumMembershipCreateXLS, username);
+    }
+  }
+
+  private void useMatchingCriteriaToCreateCurriculumMembership(
+      CurriculumMembershipCreateXLS curriculumMembershipCreateXLS, String username) {
+    String tisProgrammeMembershipId = curriculumMembershipCreateXLS.getTisProgrammeMembershipId();
+    if (!StringUtils.isEmpty(tisProgrammeMembershipId)) {
+      try {
+        ProgrammeMembershipDTO programmeMembershipDTO = tcsServiceImpl.getProgrammeMembershipByUuid(
+            UUID.fromString(tisProgrammeMembershipId));
+        if (programmeMembershipDTO != null) {
+          //need to call the create curriculum membership method here
+        } else {
+          curriculumMembershipCreateXLS.addErrorMessage(
+              String.format(DID_NOT_FIND_PROGRAMME_MEMBERSHIP_FOR_ID, tisProgrammeMembershipId));
+        }
+      } catch (ResourceAccessException e) {
+        new ErrorHandler().recordErrorMessageOnTemplateOrLogUnknown(curriculumMembershipCreateXLS,
+            e);
+      }
+    }
+  }
 }
