a9500d3f2ff6df01d763c8482308411543549bb6
Cai.Willis <cai.willis@hee.nhs.uk>
2024-12-20 16:02:48 +0000

    wip: validation and check for existing programme membership

--- generic-upload-service/src/main/java/com/transformuk/hee/tis/genericupload/service/service/CurriculumMembershipCreateTransformerService.java
+++ generic-upload-service/src/main/java/com/transformuk/hee/tis/genericupload/service/service/CurriculumMembershipCreateTransformerService.java
@@ -1,8 +1,11 @@
 package com.transformuk.hee.tis.genericupload.service.service;
 
 import com.transformuk.hee.tis.genericupload.api.dto.CurriculumMembershipCreateXLS;
+import com.transformuk.hee.tis.tcs.api.dto.CurriculumMembershipDTO;
+import com.transformuk.hee.tis.tcs.api.dto.ProgrammeDTO;
 import com.transformuk.hee.tis.tcs.api.dto.ProgrammeMembershipDTO;
 import com.transformuk.hee.tis.tcs.client.service.impl.TcsServiceImpl;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,38 +16,84 @@ import org.springframework.web.client.ResourceAccessException;
 @Component
 public class CurriculumMembershipCreateTransformerService {
 
-  private static final String DID_NOT_FIND_PROGRAMME_MEMBERSHIP_FOR_ID = "Did not find the programme membership for id \"%s\".";
+  private static final String NO_PROGRAMME_MEMBERSHIP_FOR_ID = "Could not find the programme membership for id \"%s\".";
 
-  @Autowired
-  private TcsServiceImpl tcsServiceImpl;
+  private static final String EMPTY_PROGRAMME_MEMBERSHIP_ID_FIELD = "Programme Membership Id field cannot be null or empty";
+
+  private static final String EMPTY_CURRICULUM_NAME_FIELD = "Curriculum Name field cannot be null or empty";
+
+  private static final String EMPTY_CURRICULUM_START_DATE_FIELD = "Curriculum Start Date field cannot be null or empty";
+
+  private static final String EMPTY_CURRICULUM_END_DATE_FIELD = "Curriculum End Date field cannot be null or empty";
+
+  private TcsServiceImpl tcsService;
+
+  private CurriculumMembershipCreateTransformerService(TcsServiceImpl tcsService) {
+    this.tcsService = tcsService;
+  }
 
   void processCurriculumMembershipCreateUpload(
-      List<CurriculumMembershipCreateXLS> curriculumMembershipCreateXLSS, String username) {
-    curriculumMembershipCreateXLSS.forEach(
-        CurriculumMembershipCreateXLS::initialiseSuccessfullyImported);
+      List<CurriculumMembershipCreateXLS> upload) {
+    upload.forEach(
+        cm -> {
+          cm.initialiseSuccessfullyImported();
+          boolean valid = validateUpload(cm);
+          ProgrammeMembershipDTO pm = null;
+          if (valid) {
+            pm = checkAndValidateExistingPmId(cm);
+          }
+          if (pm != null) {
+            cm.setSuccessfullyImported(false);
+            return;
+          }
+          createcurriculumMembershipXls(cm);
+          cm.setSuccessfullyImported(true);
+        });
+  }
 
-    for (CurriculumMembershipCreateXLS curriculumMembershipCreateXLS : curriculumMembershipCreateXLSS) {
-      useMatchingCriteriaToCreateCurriculumMembership(curriculumMembershipCreateXLS, username);
+  private boolean validateUpload(
+      CurriculumMembershipCreateXLS curriculumMembershipXls) {
+    if (curriculumMembershipXls.getTisProgrammeMembershipId() == null
+        || curriculumMembershipXls.getTisProgrammeMembershipId().trim().isEmpty()) {
+      curriculumMembershipXls.addErrorMessage(EMPTY_PROGRAMME_MEMBERSHIP_ID_FIELD);
+      return false;
+    }
+    if (curriculumMembershipXls.getCurriculumName() == null
+        || curriculumMembershipXls.getCurriculumName().trim().isEmpty()) {
+      curriculumMembershipXls.addErrorMessage(EMPTY_CURRICULUM_NAME_FIELD);
+      return false;
+    }
+    if (curriculumMembershipXls.getCurriculumStartDate() == null) {
+      curriculumMembershipXls.addErrorMessage(EMPTY_CURRICULUM_START_DATE_FIELD);
+      return false;
+    }
+    if (curriculumMembershipXls.getCurriculumEndDate() == null) {
+      curriculumMembershipXls.addErrorMessage(EMPTY_CURRICULUM_END_DATE_FIELD);
+      return false;
+    }
+    return true;
+  }
+
+  private ProgrammeMembershipDTO checkAndValidateExistingPmId(
+      CurriculumMembershipCreateXLS curriculumMembershipXls) {
+    try {
+      ProgrammeMembershipDTO programmeMembership = tcsService.getProgrammeMembershipByUuid(
+          UUID.fromString(curriculumMembershipXls.getTisProgrammeMembershipId()));
+      if (programmeMembership == null) {
+        curriculumMembershipXls.addErrorMessage(NO_PROGRAMME_MEMBERSHIP_FOR_ID);
+      }
+      return programmeMembership;
+    } catch (Exception e) {
+      curriculumMembershipXls.addErrorMessage(e.getMessage());
+      return null;
     }
   }
 
-  private void useMatchingCriteriaToCreateCurriculumMembership(
-      CurriculumMembershipCreateXLS curriculumMembershipCreateXLS, String username) {
-    String tisProgrammeMembershipId = curriculumMembershipCreateXLS.getTisProgrammeMembershipId();
-    if (!StringUtils.isEmpty(tisProgrammeMembershipId)) {
-      try {
-        ProgrammeMembershipDTO programmeMembershipDTO = tcsServiceImpl.getProgrammeMembershipByUuid(
-            UUID.fromString(tisProgrammeMembershipId));
-        if (programmeMembershipDTO != null) {
-          //need to call the create curriculum membership method here
-        } else {
-          curriculumMembershipCreateXLS.addErrorMessage(
-              String.format(DID_NOT_FIND_PROGRAMME_MEMBERSHIP_FOR_ID, tisProgrammeMembershipId));
-        }
-      } catch (ResourceAccessException e) {
-        new ErrorHandler().recordErrorMessageOnTemplateOrLogUnknown(curriculumMembershipCreateXLS,
-            e);
-      }
-    }
+  private void createcurriculumMembershipXls(
+      CurriculumMembershipCreateXLS curriculumMembershipXls) {
+    CurriculumMembershipDTO curriculumMembershipDTO = new CurriculumMembershipDTO();
+    curriculumMembershipDTO.setCurriculumId();
+    curriculumMembershipDTO.setCurriculumStartDate();
+    curriculumMembershipDTO.setCurriculumEndDate();
   }
 }
--- generic-upload-service/src/test/java/com/transformuk/hee/tis/genericupload/service/service/CurriculumMembershipCreateTransformerServiceTest.java
+++ generic-upload-service/src/test/java/com/transformuk/hee/tis/genericupload/service/service/CurriculumMembershipCreateTransformerServiceTest.java
@@ -1,27 +1,57 @@
 package com.transformuk.hee.tis.genericupload.service.service;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import com.transformuk.hee.tis.genericupload.api.dto.CurriculumMembershipCreateXLS;
+import com.transformuk.hee.tis.tcs.api.dto.ProgrammeMembershipDTO;
+import com.transformuk.hee.tis.tcs.client.service.impl.TcsServiceImpl;
 import java.time.LocalDate;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@RunWith(MockitoJUnitRunner.class)
-public class CurriculumMembershipCreateTransformerServiceTest {
+@ExtendWith(MockitoExtension.class)
+class CurriculumMembershipCreateTransformerServiceTest {
 
+  private static final String NO_PROGRAMME_MEMBERSHIP_FOR_ID = "Could not find the programme membership for id \"%s\".";
+
+  private static final String EMPTY_PROGRAMME_MEMBERSHIP_ID_FIELD = "Programme Membership Id field cannot be null or empty";
+
+  private static final String EMPTY_CURRICULUM_NAME_FIELD = "Curriculum Name field cannot be null or empty";
+
+  private static final String EMPTY_CURRICULUM_START_DATE_FIELD = "Curriculum Start Date field cannot be null or empty";
+
+  private static final String EMPTY_CURRICULUM_END_DATE_FIELD = "Curriculum End Date field cannot be null or empty";
   @InjectMocks
   CurriculumMembershipCreateTransformerService service;
+
+  @Mock
+  TcsServiceImpl tcsService;
+
+  @Captor
+  ArgumentCaptor<ProgrammeMembershipDTO> pmCaptor;
+
   private CurriculumMembershipCreateXLS xlsValid, xlsInvalid1, xlsInvalid2, xlsInvalid3, xlsInvalid4;
-  private final String TIS_PROGRAMME_MEMBERSHIP_ID = "1";
+  private final UUID TIS_PROGRAMME_MEMBERSHIP_ID = UUID.randomUUID();
   private final String CURRICULUM_NAME = "curriculum1";
   private final LocalDate CURRICULUM_START_DATE = LocalDate.now().minusDays(1);
   private final LocalDate CURRICULUM_END_DATE = LocalDate.now().plusDays(1);
-  @Before
+
+  @BeforeEach
   void setUp() {
     xlsValid = new CurriculumMembershipCreateXLS();
-    xlsValid.setTisProgrammeMembershipId(TIS_PROGRAMME_MEMBERSHIP_ID);
+    xlsValid.setTisProgrammeMembershipId(TIS_PROGRAMME_MEMBERSHIP_ID.toString());
     xlsValid.setCurriculumName(CURRICULUM_NAME);
     xlsValid.setCurriculumStartDate(CURRICULUM_START_DATE);
     xlsValid.setCurriculumEndDate(CURRICULUM_END_DATE);
@@ -34,40 +64,84 @@ public class CurriculumMembershipCreateTransformerServiceTest {
 
     // missing curriculumName
     xlsInvalid2 = new CurriculumMembershipCreateXLS();
-    xlsInvalid2.setTisProgrammeMembershipId(TIS_PROGRAMME_MEMBERSHIP_ID);
+    xlsInvalid2.setTisProgrammeMembershipId(TIS_PROGRAMME_MEMBERSHIP_ID.toString());
     xlsInvalid2.setCurriculumStartDate(CURRICULUM_START_DATE);
     xlsInvalid2.setCurriculumEndDate(CURRICULUM_END_DATE);
 
     // missing curriculumStartDate
     xlsInvalid3 = new CurriculumMembershipCreateXLS();
-    xlsInvalid3.setTisProgrammeMembershipId(TIS_PROGRAMME_MEMBERSHIP_ID);
+    xlsInvalid3.setTisProgrammeMembershipId(TIS_PROGRAMME_MEMBERSHIP_ID.toString());
     xlsInvalid3.setCurriculumName(CURRICULUM_NAME);
     xlsInvalid3.setCurriculumEndDate(CURRICULUM_END_DATE);
 
     // missing curriculumEndDate
     xlsInvalid4 = new CurriculumMembershipCreateXLS();
-    xlsInvalid4.setTisProgrammeMembershipId(TIS_PROGRAMME_MEMBERSHIP_ID);
+    xlsInvalid4.setTisProgrammeMembershipId(TIS_PROGRAMME_MEMBERSHIP_ID.toString());
     xlsInvalid4.setCurriculumName(CURRICULUM_NAME);
     xlsInvalid4.setCurriculumStartDate(CURRICULUM_START_DATE);
   }
 
   @Test
   void shouldFailValidationIfTisProgrammeMembershipIdNotProvided() {
+    List<CurriculumMembershipCreateXLS> upload = Arrays.asList(xlsInvalid1);
 
+    service.processCurriculumMembershipCreateUpload(upload);
+
+    assertEquals(EMPTY_PROGRAMME_MEMBERSHIP_ID_FIELD, xlsInvalid1.getErrorMessage());
   }
 
   @Test
   void shouldFailValidationIfCurriculumNameNotProvided() {
+    List<CurriculumMembershipCreateXLS> upload = Arrays.asList(xlsInvalid2);
 
+    service.processCurriculumMembershipCreateUpload(upload);
+
+    assertEquals(EMPTY_CURRICULUM_NAME_FIELD, xlsInvalid2.getErrorMessage());
   }
 
   @Test
   void shouldFailValidationIfCurriculumStartDateNotProvided() {
+    List<CurriculumMembershipCreateXLS> upload = Arrays.asList(xlsInvalid3);
 
+    service.processCurriculumMembershipCreateUpload(upload);
+
+    assertEquals(EMPTY_CURRICULUM_START_DATE_FIELD, xlsInvalid3.getErrorMessage());
   }
 
   @Test
   void shouldFailValidationIfCurriculumEndDateNotProvided() {
+    List<CurriculumMembershipCreateXLS> upload = Arrays.asList(xlsInvalid4);
 
+    service.processCurriculumMembershipCreateUpload(upload);
+
+    assertEquals( EMPTY_CURRICULUM_END_DATE_FIELD, xlsInvalid4.getErrorMessage());
+  }
+
+  @Test
+  void shouldReportErrorIfProgrammeMembershipNotFoundWithGivenId() {
+    List<CurriculumMembershipCreateXLS> upload = Arrays.asList(xlsValid);
+
+    when(tcsService.getProgrammeMembershipByUuid(TIS_PROGRAMME_MEMBERSHIP_ID)).thenReturn(null);
+    service.processCurriculumMembershipCreateUpload(upload);
+
+    assertEquals(NO_PROGRAMME_MEMBERSHIP_FOR_ID, xlsValid.getErrorMessage());
+  }
+
+  @Test
+  void shouldProcessValidUpload() {
+    List<CurriculumMembershipCreateXLS> upload = Arrays.asList(xlsValid);
+
+    ProgrammeMembershipDTO programmeMembershipDTO = new ProgrammeMembershipDTO();
+    programmeMembershipDTO.setUuid(UUID.fromString(xlsValid.getTisProgrammeMembershipId()));
+
+    when(tcsService.getProgrammeMembershipByUuid(TIS_PROGRAMME_MEMBERSHIP_ID)).thenReturn(
+        programmeMembershipDTO);
+    service.processCurriculumMembershipCreateUpload(upload);
+
+    verify(tcsService.updateProgrammeMembership(pmCaptor.capture()));
+
+    assertEquals("1", pmCaptor.getValue().getCurriculumMemberships().get(0).getCurriculumId());
+    assertEquals(null, xlsValid.getErrorMessage());
+    assertEquals(true, xlsValid.isSuccessfullyImported());
   }
 }
