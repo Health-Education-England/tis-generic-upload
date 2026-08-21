1e9d1f2b78e00a11f9f1860bc53e583219131f1e
jayanta2018 <jayanta.saha@hee.nhs.uk>
2026-08-21 12:08:38 +0100

    chore: improve logging

--- generic-upload-service/src/main/java/com/transformuk/hee/tis/genericupload/service/service/PersonTransformerService.java
+++ generic-upload-service/src/main/java/com/transformuk/hee/tis/genericupload/service/service/PersonTransformerService.java
@@ -102,7 +102,7 @@ public class PersonTransformerService {
       "A valid programme membership is needed to add a rotation";
   private static final String ADDRESS_FIELD_REQUIRED = "%1$s is required when %2$s is populated.";
   private static final String VALIDATE_EMAIL_ERROR = "Valid email address required.";
-  private static final Logger log = LoggerFactory.getLogger(PersonTransformerService.class);
+  private static final Logger logger = LoggerFactory.getLogger(PersonTransformerService.class);
 
   @Autowired
   private TcsServiceImpl tcsServiceImpl;
@@ -321,10 +321,13 @@ public class PersonTransformerService {
   private void updateOrRecordError(PersonDTO personDTOFromDB, PersonDTO personDTOFromXLS,
       PersonXLS personXLS) {
     try {
+      logger.info("Sending People bulk update request to TCS: personId={}", personDTOFromDB.getId());
       personDTOFromDB = tcsServiceImpl.updatePersonForBulkWithAssociatedDTOs(personDTOFromDB);
+      logger.info("People bulk update request completed successfully in TCS: personId={}",
+          personDTOFromDB.getId());
       addQualificationsRotationsAndProgrammeMemberships(personXLS, personDTOFromXLS,
           personDTOFromDB);
-      // update TrainerApprvoal when the roles are merged
+      // update TrainerApproval when the roles are merged
       updateTrainerApproval(personDTOFromDB);
       if (StringUtils.isEmpty(personXLS.getErrorMessage())) {
         personXLS.setSuccessfullyImported(true);
@@ -481,7 +484,9 @@ public class PersonTransformerService {
     PersonDTO personDTO = getPersonDTO(personXLS);
     if (personDTO != null) {
       try {
+        logger.info("Sending create person request to TCS: personId={}", personDTO.getId());
         PersonDTO savedPersonDTO = tcsServiceImpl.createPerson(personDTO);
+        logger.info("Create person request completed successfully in TCS: personId={}", savedPersonDTO.getId());
         addQualificationsRotationsAndProgrammeMemberships(personXLS, personDTO, savedPersonDTO);
         addTrainerApproval(savedPersonDTO);
       } catch (ResourceAccessException rae) {
@@ -510,7 +515,7 @@ public class PersonTransformerService {
         CurriculumDTO tcsCurriculum1 = getCurriculumDtoFromTcs(personXLS.getCurriculum1());
         CurriculumDTO tcsCurriculum2 = getCurriculumDtoFromTcs(personXLS.getCurriculum2());
         CurriculumDTO tcsCurriculum3 = getCurriculumDtoFromTcs(personXLS.getCurriculum3());
-        log.debug(
+        logger.debug(
             "Evaluating pathway for Programme={}, curriculum1={}, curriculum2={}, curriculum3={}",
             programmeMembershipDTO, tcsCurriculum1, tcsCurriculum2, tcsCurriculum3);
         evaluateTrainingPathway(programmeMembershipDTO, tcsCurriculum1, tcsCurriculum2,
--- generic-upload-service/src/main/java/com/transformuk/hee/tis/genericupload/service/service/PlacementDeleteService.java
+++ generic-upload-service/src/main/java/com/transformuk/hee/tis/genericupload/service/service/PlacementDeleteService.java
@@ -1,8 +1,11 @@
 package com.transformuk.hee.tis.genericupload.service.service;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 import com.transformuk.hee.tis.genericupload.api.dto.PlacementDeleteXLS;
 import com.transformuk.hee.tis.tcs.client.service.impl.TcsServiceImpl;
 import java.util.List;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.ResourceAccessException;
@@ -10,6 +13,7 @@ import org.springframework.web.client.ResourceAccessException;
 @Component
 public class PlacementDeleteService {
 
+  private static final Logger logger = getLogger(PlacementDeleteService.class);
   @Autowired
   private TcsServiceImpl tcsServiceImpl;
 
@@ -17,7 +21,11 @@ public class PlacementDeleteService {
     placementDeleteXLSS.forEach(placementDeleteXLS -> {
       if ("DELETE".equalsIgnoreCase(placementDeleteXLS.getPlacementStatus())) {
         try {
+          logger.info("Sending delete placement request to TCS: placementId={}",
+              placementDeleteXLS.getPlacementId());
           tcsServiceImpl.deletePlacement(Long.valueOf(placementDeleteXLS.getPlacementId()));
+          logger.info("Delete placement request completed successfully in TCS: placementId={}",
+              placementDeleteXLS.getPlacementId());
           placementDeleteXLS.setSuccessfullyImported(true);
         } catch (ResourceAccessException rae) {
           new ErrorHandler().recordErrorMessageOnTemplateOrLogUnknown(placementDeleteXLS, rae);
